0ace32f3ad38072d0d52e07b6bce9912150fc395
Lachlan <lachlan.miller.1990@outlook.com>
2018-04-12 13:59:11 +0900

    Add song and measure

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 import Timer from './timer'
+import Song from './song'
 
-const timer: Timer = new Timer(true)
+const song: Song = new Song()
+console.log(song.beatsPerSecond)
+
+for (const m in song.measures) {
+  console.log(song.measures[m])
+}
+// const timer: Timer = new Timer(true)
--- song.ts
+++ song.ts
@@ -1,3 +1,23 @@
 class Song {
   bpm: number = 180
+  beatsPerSecond: number = this.bpm / 60
+
+  measures: string[][] = [
+    [
+      '10000000',
+      '10000010',
+      '10010000',
+      '10000110',
+    ],
+    [
+      '00000110',
+      '00110010',
+      '00110000',
+      '00100010',
+    ],
+  ]
+
+  cosntructor() {}
 }
+
+export default Song
