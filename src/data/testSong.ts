29579da8335c721f7ed91e5c5b31bca018fab7d1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-03-28 23:52:36 +1000

    Add missing id to test song notes

--- src/data/testSong.ts
+++ src/data/testSong.ts
@@ -5,14 +5,17 @@ const mySong: ISong = {
   bpm: 60,
   notes: [
     {
+      id: '1000',
       timestamp: 1000,
       touchedAt: null
     },
     {
+      id: '3000',
       timestamp: 3000,
       touchedAt: null
     },
     {
+      id: '3500',
       timestamp: 3500,
       touchedAt: null
     }
